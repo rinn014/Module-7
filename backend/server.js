a36272dcccc3fc3f6b353e60d28e1776dc80b41c
janrold-devs <janroldvandeguinto18@gmail.com>
2025-09-17 22:25:11 +0800

    Inventory API done

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,14 @@ const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
 
+//Import Routes
+const inventoryRoutes = require("./routes/inventory.routes");
+const transactionRoutes = require("./routes/transaction.routes");
+const warehouseRoutes = require("./routes/warehouse.routes");
+
 const app = express();
 
+//Middleware
 app.use(
     cors({
         origin: process.env.CLIENT_URL || "*",
@@ -15,7 +21,14 @@ app.use(
 
 app.use(express.json());
 
+//Connect to Database
 connectDB();
 
+//Routes
+app.use("/api/inventory", inventoryRoutes);
+app.use("/api/transactions", transactionRoutes);
+app.use("/api/warehouses", warehouseRoutes);
+
+//Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
