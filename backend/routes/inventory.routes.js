23a99c79220b0f14ae962c3ea1affb9361a6d82d
janrold-devs <janroldvandeguinto18@gmail.com>
2025-09-17 22:13:46 +0800

    Added inventory routes

--- backend/routes/inventory.routes.js
+++ backend/routes/inventory.routes.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const inventoryController = require("../controllers/inventory.controller");
 
 // Inventory CRUD
-router.post("/add", inventoryController.addItem);          // Create new item
-router.put("/update/:id", inventoryController.updateItem); // Update item
-router.get("/get-items", inventoryController.getAllItems);          // Get all items
-router.get("/get-item/:id", inventoryController.getItemById);       // Get item by ID
-router.delete("/delete-item/:id", inventoryController.deleteItem);     // Delete item
+router.post("/addItem", inventoryController.addItem);          // Create new item
+router.put("/updateItem/:id", inventoryController.updateItem); // Update item
+router.get("/getItems", inventoryController.getAllItems);          // Get all items
+router.get("/getItem/:id", inventoryController.getItemById);       // Get item by ID
+router.delete("/deleteItem/:id", inventoryController.deleteItem);     // Delete item
 
 module.exports = router;
