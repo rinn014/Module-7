95fce2a6bc1c4f55725691f42bdc87f8716d3629
janrold-devs <janroldvandeguinto18@gmail.com>
2025-09-18 12:26:06 +0800

    Transaction model can be linked to module 3

--- backend/models/Transaction.js
+++ backend/models/Transaction.js
@@ -6,7 +6,13 @@ const TransactionSchema = new mongoose.Schema({
   quantity: { type: Number, required: true },
   transactionDate: { type: Date, default: Date.now },
   remarks: String,
-  expiryDate: Date
+  expiryDate: Date,
+
+  purchaseOrderId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "PurchaseOrder", // Comes from Module 3
+    default: null,
+  },
 });
 
 module.exports = mongoose.model("Transaction", TransactionSchema);
