5dc6bea43267f966d5f52637ba9e065d888e17a4
janrold-devs <janroldvandeguinto18@gmail.com>
2025-09-19 15:27:20 +0800

    added folders on frontend to make it clean

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Procurement from "./pages/Dashboard/Procurement"
+import Procurement from "./pages/Dashboard/Procurement";
 
 const App = () => {
   return (
