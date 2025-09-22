1e8a3fc0c6219cf279c1db215240754129f15b81
zenniaa <ortizzenniajane@gmail.com>
2025-09-22 23:51:11 +0800

    Cleaned vite.config.js after merge

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(),tailwindcss(),],
-
-})
-=======
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
@@ -17,4 +6,3 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 });
->>>>>>> module_1
