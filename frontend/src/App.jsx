89c98652eb60b3604f4c34d5b38fce8afb30202d
janrold-devs <janroldvandeguinto18@gmail.com>
2025-09-29 17:20:41 +0800

    Initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Procurement from "./pages/Dashboard/Procurement";
+import Suppliers from "./pages/Procurement/Suppliers";
+import Requisition from "./pages/Procurement/Requisition";
+import PurchaseOrders from "./pages/Procurement/PurchaseOrders";
+import Invoices from "./pages/Procurement/Invoices";
+
 import Inventory from "./pages/Dashboard/Inventory";
 import Transaction from "./pages/Dashboard/Transaction";
 import Warehouse from "./pages/Dashboard/Warehouse";
@@ -14,6 +20,11 @@ const App = () => {
           <Route path="/inventory" element={<Inventory />} />
           <Route path="/transactions" element={<Transaction />} />
           <Route path="/warehouse" element={<Warehouse />} />
+          <Route path="/procurement" element={<Procurement />} />
+          <Route path="/procurement/suppliers" element={<Suppliers />} />
+          <Route path="/procurement/requisition" element={<Requisition />} />
+          <Route path="/procurement/purchase-orders" element={<PurchaseOrders />} />
+          <Route path="/procurement/invoices" element={<Invoices />} />
         </Routes>
       </Router>
     </div>
