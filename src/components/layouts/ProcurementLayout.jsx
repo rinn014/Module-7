f2c6313676b0fd60f872710a67c8712c16262512
rinn014 <redshadow095@gmail.com>
2025-10-21 16:28:14 +0800

    Delete src directory

--- src/components/layouts/ProcurementLayout.jsx
+++
@@ -1,9 +0,0 @@
-// frontend/src/components/layouts/ProcurementLayout.jsx
-import React from "react";
-import { Link } from "react-router-dom";
-
-const ProcurementLayout = ({ children }) => {
-  
-}
-
-export default ProcurementLayout;
