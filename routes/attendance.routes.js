82b8cc39227402655cbf5cb8acf3051abe90097c
rinn014 <redshadow095@gmail.com>
2025-10-21 16:28:04 +0800

    Delete routes directory

--- routes/attendance.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("../controllers/attendance.controller");
-
-router.get("/", controller.getAll);
-router.post("/", controller.create);
-router.patch("/:id", controller.update);
-
-module.exports = router;
--- routes/leave.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("../controllers/leave.controller");
-
-router.get("/", controller.getAll);
-router.post("/", controller.create);
-
-module.exports = router;
